0d969be56b6b482eb4a3d9c5ad5ddb4c78e1a064
alwaysbritt24 <bslee05@icloud.com>
2025-09-18 14:52:33 -0400

    Initial Commit

---
+++ World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  World
+//
+//  Created by Brittany on 9/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldTests/WorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTests.swift
+//  WorldTests
+//
+//  Created by Brittany on 9/18/25.
+//
+
+import Testing
+@testable import World
+
+struct WorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
