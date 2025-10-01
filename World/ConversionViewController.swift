75158ad31be04326b5c280b4e4bbec88e6659722
alwaysbritt24 <bslee05@icloud.com>
2025-10-01 14:16:02 -0400

    text input

--- World/ConversionViewController.swift
+++ World/ConversionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ConversionViewController: UIViewController {
 
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,20 @@ class ConversionViewController: UIViewController {
     }
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+        if let text = textField.text, !text.isEmpty {
+                celsiusLabel.text = text
+            } else {
+                celsiusLabel.text = "???"
+            }
+   }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
+
 }
+
+        
+        
+    
+
